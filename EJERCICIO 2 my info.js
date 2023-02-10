79250e703960e172800e8d9d24e94f6097db70ac
alexanderrami <124542430+alexanderrami@users.noreply.github.com>
2023-02-09 20:03:04 -0500

    Add files via upload

---
+++ EJERCICIO 2 my info.js
@@ -0,0 +1,16 @@
+const myInfo = [
+    {
+      name: "Alexander",
+      age: 26,
+      isDeveloper: false,
+      fecha: new Date("1996-06-16"),
+      favoriteBook: {
+        title: "la guerra de los mil dias",
+        author: "Jose Mauel Marroquin",
+        publicationDate: new Date("1899-17-10"),
+        url: "https://www.canalinstitucional.tv/especial/guerra-mil-dias-batallas"
+      }
+    }
+  ];
+  
+  console.log(myInfo);
